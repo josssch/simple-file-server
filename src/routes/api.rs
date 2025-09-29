35cecd77c5685e2f8ab0627508057aa8115e825f
Josh <hi@jossh.dev>
2025-09-29 16:03:42 -0400

    feat: initial file removal endpoint

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -3,7 +3,10 @@ use actix_web::{Scope, dev::HttpServiceFactory, middleware};
 
 use crate::{
     authorized::is_authorized,
-    routes::{ScopeCreator, upload_file::upload_file},
+    routes::{
+        ScopeCreator,
+        upload_file::{delete_file, upload_file},
+    },
 };
 
 pub struct ApiRoute;
@@ -14,5 +17,6 @@ impl ScopeCreator for ApiRoute {
             .app_data(MultipartFormConfig::default())
             .wrap(middleware::from_fn(is_authorized))
             .service(upload_file)
+            .service(delete_file)
     }
 }
